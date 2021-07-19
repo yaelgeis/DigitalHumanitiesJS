1006ea8c6d5ec2419a5abb910c13eb48d0735b82
Yael Geisler <yaeli21995@gmail.com>
2021-07-19 21:25:10 +0300

    filters working

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,47 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
+  let [data, setData] = React.useState(null);
   React.useEffect(() => {
-    fetch("/filter")
+
+    function filterByStr (col, str){
+      const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify({filterStr: true, col: `${col}`, str: `${str}` }),
+        headers: {"Content-Type": "application/json"}
+      }; 
+      console.log(requestOptions);
+      return requestOptions;
+    }
+
+    const requestOptions =  filterByStr('כותרת', 'יעל');
+
+    fetch(`/filter`, requestOptions)
       .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+      .then((data) => setData(data.items))
+    }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <p>{!data ? "Loading..." : JSON.stringify(data)}</p>
       </header>
     </div>
   );
+
 }
+
+// function simplify(data){
+//   console.log("type = " + typeof(data)); 
+//   console.log(data);
+//   let str = "[";
+//   data.foreach((s) => str = str + s.toSring() +", ");
+//   str = str.substr(0, str.length-2)
+//   str += "]"
+//   return str;
+
+// }
+
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -6,14 +6,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// app.get("/api", (req, res) => {
-//     res.json({ message: "Hello from server!" });
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server listening on ${PORT}`);
-// });
-
+app.use(express.json());
+app.use(express.urlencoded());
 
 const dbURI = "mongodb+srv://Digital:igaleyaelgeis@cluster0.qofyt.mongodb.net/Recently-Uploaded-Files?retryWrites=true&w=majority";
 
@@ -21,40 +15,55 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 mongoose.connect(dbURI);
 
-app.get("/filter", (req, res) => { 
-    filter(req,res);
+var schema = new Schema(
+    { 
+        סוג_התיק: String,
+        מיקום_גוף_יוצר:String,
+        מספר_תיק_ישן:String, 
+        סטטוס_חשיפה:String,
+        מספר_מסמכים_בתיק:String,
+        גופים:String,
+        תקופת_החומר_עד:String,
+        תקופת_החומר_מ:String,
+        תיאור:String,
+        אישים:String,
+        כותרת:String,
+        קישור:String 
+    }, 
+    {collection : 'yael-try'}
+);
+
+app.post("/filter", (req, res) => { 
+    filterTable(req,res);
 });
+
+
+
+function filterTable (req, res) {
+    const model = mongoose.model('question', schema);
+
+    if (req.body.filterStr)
+        filterStr(model, req.body.col, req.body.str, res);
+    else
+        filterDate(model, req.body.start, req.body.end, res);    
+}
+
+async function filterStr(model, col, str, res){
+    const regex = new RegExp(str, 'i');
+    let findExp = {};
+    findExp[col] = {$regex: regex};
+    const items = await model.find(findExp, {"_id":0});
+    res.send({items: items});
+}
+
+async function filterDate(model, start, end, res){
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    const findExp = {'תקופת החומר מ':  {"$gte": startDate}, 'תקופת החומר עד':  {"$lte": endDate} }
+    const items = await model.find(findExp, {"_id":0});
+    res.send({items: items});
+}
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
-
-async function filter (req, res) {
-    let questions = mongoose.model('question', 
-        new Schema({ 
-            סוג_התיק: String,
-            מיקום_גוף_יוצר:String,
-            מספר_תיק_ישן:String, 
-            סטטוס_חשיפה:String,
-            מספר_מסמכים_בתיק:String,
-            גופים:String,
-            תקופת_החומר_עד:String,
-            תקופת_החומר_מ:String,
-            תיאור:String,
-            אישים:String,
-            כותרת:String,
-            קישור:String 
-        },
-        {collection : 'June 2021'})
-    );
-
-
-    const str1 = 'יצחק בן-צבי';
-    const regex = new RegExp(str1, 'i');
-    const x = await questions.find({אישים: {$regex: regex}});
-    res.json( {message: `${x}`})
-
-    // x.then(()=> res.json( {message: `${x}`}));
-    
-    
-}
